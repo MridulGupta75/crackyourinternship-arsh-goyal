4fb94862282efcb083a92360041b4c97218d2af1
MridulGupta75 <66024896+MridulGupta75@users.noreply.github.com>
2022-05-09 18:17:50 +0530

    Time: 4 ms (26.23%), Space: 7 MB (7.40%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,73 +1,77 @@
 // class Solution {
-//       vector<string>c;
+//     vector<string>sol;
 // public:
-//     //vector<pair<int,string>>
-//     void getcombo(string combo[],string digit,int i,string s)
+//     void getcombo(string arr[],int digits,int i,string s)
 //     {
-//         if(i==digit.length())
+//         if(i==digits.length())
 //         {
-//             c.push_back(s);
+//             sol.push_back(s);
 //             return;
 //         }
 //         else
 //         {
-//           // string str=combo[(digit[i]-'0')-2].second;
-//             string str=combo[digit[i]-'0'];
-//             for(int j=0;j<str.length();i++)
-//             getcombo(combo,digit,i+1,s+str[j]);
+//             string str=arr[digits[i]-'0'];
+//             for(int j=0;j<str.length();j++)
 //         }
 //     }
 //     vector<string> letterCombinations(string digits) {
-//         // vector<pair<int,string>>combo;
 //         if(digits.length()==0)return c;
-//         // int x=97;
-//         // for(int i=2;i<=9;i++)
-//         // {
-//         //     int m;
-//         //     string str="";
-//         //     if(i==7||i==9)
-//         //     {
-//         //         m=4;
-//         //         while(m--){
-//         //         str+=(char)x;
-//         //              x++;}
-//         //         combo.push_back({i,str}); }
-//         //     else {
-//         //         m=3;
-//         //         while(m--){
-//         //             str+=(char)x;
-//         //             x++;}
-//         //          combo.push_back({i,str});} }
-// //         for(int i=0;i<combo.size();i++)
-// //         {
-// //             cout<<i<<" "<<combo[i].first<<" "<<combo[i].second<<endl;
-// //         }
-//         string combo[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-//         getcombo(combo,digits,0,"");
-//             return c;
+//         else
+//         {
+//             string arr[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//             getcombo(arr,digits,0,"");
+//             return sol;
+//         }
+        
 //     }
-    
 // };
 class Solution {
+      vector<string>c;
 public:
-    void myLetterComb(string digits, vector<string> &res, string nums[], int i, string s){
-        if(i == digits.length()){
-            res.push_back(s);
+    
+    void getcombo(vector<pair<int,string>> combo,string digit,int i,string s)
+    {
+        if(i==digit.length())
+        {
+            c.push_back(s);
             return;
         }
-        string val = nums[digits[i] - '0'];
-        for(int j=0;j<val.length(); j++)
-            myLetterComb(digits, res, nums, i+1, s+val[j]);
+        else
+        {
+           string str=combo[(digit[i]-'0')-2].second;
+            // string str=combo[digit[i]-'0'];
+            for(int j=0;j<str.length();j++)
+            getcombo(combo,digit,i+1,s+str[j]);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+         vector<pair<int,string>>combo;
+        if(digits.length()==0)return c;
+        int x=97;
+        for(int i=2;i<=9;i++)
+        {
+            int m;
+            string str="";
+            if(i==7||i==9)
+            {
+                m=4;
+                while(m--){
+                str+=(char)x;
+                     x++;}
+                combo.push_back({i,str}); }
+            else {
+                m=3;
+                while(m--){
+                    str+=(char)x;
+                    x++;}
+                 combo.push_back({i,str});} }
+//         for(int i=0;i<combo.size();i++)
+//         {
+//             cout<<i<<" "<<combo[i].first<<" "<<combo[i].second<<endl;
+//         }
+       // string combo[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        getcombo(combo,digits,0,"");
+            return c;
     }
     
-    vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        if(digits.length() == 0)
-            return res;
-        string nums[10] = {
-            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-        };
-        myLetterComb(digits, res, nums, 0, "");
-        return res;
-    }
 };
